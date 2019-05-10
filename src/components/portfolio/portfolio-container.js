fa1f475fc07a0b5d2debffb3abdafefe9694ce42
Jacob Batterman <ctrjcb@gmail.com>
2019-05-10 13:27:29 -0600

    updated a few things...

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -6,15 +6,24 @@ export default class PortfolioContainer extends Component {
     constructor() {
         super();
 
-        
         console.log("Portfolio container has rendered")
     }
+
+    PortfolioItems(){
+        const data = ["Quip", "Eventbrite", "Ministry Safe", "SwingAway"];
+
+
+        return data.map(item => {
+            return <PortfolioItem title={item} url={"google.com"}/>;
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Portfolio item here...</h2>
                 
-                <PortfolioItem />
+                {this.PortfolioItems()}
             </div>
         );
     }
