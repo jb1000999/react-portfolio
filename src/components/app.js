06cdfa0e7dce97acb20c99197396f25ae82615b3
Jacob Batterman <ctrjcb@gmail.com>
2019-05-10 10:58:08 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import moment from "moment"
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Jacob Batterman Portfolio</h1>
+        <div>
+        {moment().format('MMMM Do YYYY, h:mm:ss a')}
+        </div>
+      </div>
+    );
+  }
+}
