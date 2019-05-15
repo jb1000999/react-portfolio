3895dc5ba813419161d6b7491e94bff97d59ac73
Jacob Batterman <ctrjcb@gmail.com>
2019-05-15 15:58:13 -0600

    refactored code

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -39,8 +39,7 @@ export default class PortfolioContainer extends Component {
 
   portfolioItems() {
     return this.state.data.map(item => {
-      console.log("portfolio item", item)
-      return <PortfolioItem key={item.id} title={item.name} url={item.url} slug={item.id} />;
+      return <PortfolioItem key={item.id} item={item} />;
     });
   }
 
