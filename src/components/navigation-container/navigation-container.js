6fc390045285bcd815f706e009ddf87398a04345
Jacob Batterman <ctrjcb@gmail.com>
2019-05-15 16:36:25 -0600

    added stylying to nav bar

--- src/components/navigation-container/navigation-container.js
+++ src/components/navigation-container/navigation-container.js
@@ -8,22 +8,33 @@ export default class NavigationComponent extends Component {
 
   render() {
     return (
-      <div>
-        <NavLink exact to="/" activeClassName="nav-link-active">
-          Home
-        </NavLink>
+      <div className="nav-wrapper">
+        <div className="left-side">
+          <div className="nav-link-wrapper">
+            <NavLink exact to="/" activeClassName="nav-link-active">
+              Home
+            </NavLink>
+          </div>
+          <div className="nav-link-wrapper">
+            <NavLink to="/about-me" activeClassName="nav-link-active">
+              About
+            </NavLink>
+          </div>
+          <div className="nav-link-wrapper">
+            <NavLink to="/contact" activeClassName="nav-link-active">
+              Contact
+            </NavLink>
+          </div>
+          <div className="nav-link-wrapper">
+            <NavLink to="/blog" activeClassName="nav-link-active">
+              Blog
+            </NavLink>
+          </div>
+        </div>
 
-        <NavLink to="/about-me" activeClassName="nav-link-active">
-          About
-        </NavLink>
-        <NavLink to="/contact" activeClassName="nav-link-active">
-          Contact
-        </NavLink>
-        <NavLink to="/blog" activeClassName="nav-link-active">
-          Blog
-        </NavLink>
-
-        {false ? <button>Add Blog</button> : null}
+        <div className="right-side">
+          Jacob Batterman
+        </div>
       </div>
     );
   }
