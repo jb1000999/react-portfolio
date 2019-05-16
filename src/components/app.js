287ecd883101a6c2405647523d4b04c3d8ece183
Jacob Batterman <ctrjcb@gmail.com>
2019-05-16 14:24:26 -0600

    added styling and cleaned up some code

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavigationContainer from "./navigation-container/navigation-container";
@@ -13,11 +12,9 @@ import NoMatch from "./pages/no-match";
 export default class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div className="container">
         <Router>
           <div>
-            <h1>Jacob Batterman Portfolio</h1>
-            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
             <NavigationContainer />
 
             <Switch>
--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -2,15 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function(props) {
-      // Data that we'll need:
-    // -background image: thumb_image_url
-    // -logo
-    // -description
-    // -id
     const { id, description, thumb_image_url, logo } = props.item;
   return (
-    <div>
-    <img src={thumb_image_url} />
+    <div className="portfolio-item-wrapper">
+      <div
+      className="portfolio-img-background"
+      style={{
+        backgroundImage: "url(" + thumb_image_url + ")"
+      }}
+      />
+
     <img src={logo} />
       <div>{description}</div>
       <Link to={`/portfolio/${id}`}>Link</Link>
