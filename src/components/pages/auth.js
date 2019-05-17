274b3a6751f1deb6d3bb79af352dd5ba625fbdc2
Jacob Batterman <ctrjcb@gmail.com>
2019-05-16 18:39:50 -0600

    added auth.js

---
+++ src/components/pages/auth.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Auth extends Component {
+    render() {
+        return (
+            <div>
+                Auth
+            </div>
+        )
+    }
+}
