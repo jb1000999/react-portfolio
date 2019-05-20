699e136c5d7932dfbd9287415b4ccdb174b5621f
Jacob Batterman <ctrjcb@gmail.com>
2019-05-20 11:58:36 -0600

    adding auth component...

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 import loginImg from "../../../static/assets/images/auth/login.jpg"
 
 export default class Auth extends Component {
-    render() {
-        return (
-            <div className="auth-page-wrapper">
-            <div 
-            className="left-column" 
-                style={{
-                    backgroundImage: `url(${loginImg})`
-                }}
-            />
-            
-            <div className="right-column">
-            <h1>Login Component goes here</h1>
-            </div>
-            </div>
-            
+  render() {
+    return (
+      <div className="auth-page-wrapper">
+      <div 
+      className="left-column" 
+        style={{
+          backgroundImage: `url(${loginImg})`
+        }}
+      />
+      
+      <div className="right-column">
+      <h1>Login Component goes here</h1>
+      </div>
+      </div>
+      
 
-        );
-    }
+    );
+  }
 }
