d5c2ea23572e767b47f334487a95f30d974a8429
Jacob Batterman <ctrjcb@gmail.com>
2019-05-20 13:50:00 -0600

    added auth.js working on adding auth

---
+++ src/components/auth/login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Login to access your dashboard</h1>
+
+
+      <form>
+        <input type="text" />
+        <input type="password"/>
+      </form>
+      </div>
+    );
+  }
+}
--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
+import Login from "../auth/login"
 import loginImg from "../../../static/assets/images/auth/login.jpg"
 
 export default class Auth extends Component {
   render() {
     return (
       <div className="auth-page-wrapper">
-      <div 
-      className="left-column" 
-        style={{
-          backgroundImage: `url(${loginImg})`
-        }}
-      />
+        <div 
+          className="left-column" 
+          style={{
+            backgroundImage: `url(${loginImg})`
+          }}
+        />
       
-      <div className="right-column">
-      <h1>Login Component goes here</h1>
-      </div>
+        <div className="right-column">
+        <Login />
+        </div>
       </div>
       
 
