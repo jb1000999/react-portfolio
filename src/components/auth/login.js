ac9d15b12c013838214bcf56b454d6a8a0807e08
Jacob Batterman <ctrjcb@gmail.com>
2019-05-20 14:24:11 -0600

    added functionality for login

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,16 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log("Handle submit", event);
+  }
+
   render() {
     return (
       <div>
-      <h1>Login to access your dashboard</h1>
+        <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
 
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
 
-      <form>
-        <input type="text" />
-        <input type="password"/>
-      </form>
+          <input
+            type="password"
+            name="password"
+            placeholder="Your password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
       </div>
     );
   }
