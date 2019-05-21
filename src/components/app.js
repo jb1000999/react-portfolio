b3722202556259f321630052141f08e0637d5e10
Jacob Batterman <ctrjcb@gmail.com>
2019-05-21 13:46:23 -0600

    finalized authorization

--- src/components/app.js
+++ src/components/app.js
@@ -1,84 +1,90 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 
-import NavigationContainer from './navigation-container/navigation-container';
-import Home from './pages/home';
-import About from './pages/about';
-import Contact from './pages/contact';
-import Blog from './pages/blog';
-import PortfolioDetail from './portfolio/portfolio-detail';
-import Auth from './pages/auth';
-import NoMatch from './pages/no-match';
+import NavigationContainer from "./navigation-container/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import Auth from "./pages/auth";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
 
     this.state = {
-      loggedInStatus: 'NOT_LOGGED_IN',
+      loggedInStatus: "NOT_LOGGED_IN"
     };
 
-    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind (this);
-    this.handleUnSuccessfulLogin = this.handleUnSuccessfulLogin.bind (this);
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+    this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
+    this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this);
   }
 
-  handleSuccessfulLogin () {
-    this.setState ({
-      loggedInStatus: 'LOGGED_IN',
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
     });
   }
 
-  handleUnSuccessfulLogin () {
-    this.setState ({
-      loggedInStatus: 'NOT_LOGGED_IN',
+  handleUnsuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
     });
   }
 
-  checkLoginStatus () {
+  handleSuccessfulLogout() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+
+  checkLoginStatus() {
     return axios
-      .get ('https://api.devcamp.space/logged_in', {
-        withCredentials: true,
+      .get("https://api.devcamp.space/logged_in", {
+        withCredentials: true
       })
-      .then (response => {
+      .then(response => {
         const loggedIn = response.data.logged_in;
         const loggedInStatus = this.state.loggedInStatus;
 
-        // If loggedIn and status LOGGED_IN => return data
-        // If loggedIn status NOT_LOGGED_IN => update state
-        // If not loggedIn and LOGGED_IN => update state
-
-        if (loggedIn && loggedInStatus === 'LOGGED_IN') {
+        if (loggedIn && loggedInStatus === "LOGGED_IN") {
           return loggedIn;
-        } else if (loggedIn && loggedInStatus === 'NOT_LOGGED_IN') {
-          this.setState ({
-            loggedInStatus: 'LOGGED_IN',
+        } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "LOGGED_IN"
           });
-        } else if (!loggedIn && loggedInStatus === 'LOGGED_IN') {
-          this.setState ({
-            loggedInStatus: 'NOT_LOGGED_IN',
+        } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN"
           });
         }
       })
-      .catch (error => {
-        console.log ('Error', error);
+      .catch(error => {
+        console.log("Error", error);
       });
   }
 
-  componentDidMount () {
-    this.checkLoginStatus ();
+  componentDidMount() {
+    this.checkLoginStatus();
   }
 
-  authorizedPages () {
+  authorizedPages() {
     return [<Route path="/blog" component={Blog} />];
   }
 
-  render () {
+  render() {
     return (
       <div className="container">
         <Router>
           <div>
-            <NavigationContainer loggedInStatus={this.state.loggedInStatus} />
+            <NavigationContainer
+              loggedInStatus={this.state.loggedInStatus}
+              handleSuccessfulLogout={this.handleSuccessfulLogout}
+            />
 
             <h2>{this.state.loggedInStatus}</h2>
 
@@ -98,9 +104,9 @@ export default class App extends Component {
 
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
-              {this.state.loggedInStatus === 'LOGGED_IN'
-                ? this.authorizedPages ()
-                : null}
+              {this.state.loggedInStatus === "LOGGED_IN" ? (
+                this.authorizedPages()
+              ) : null}
               <Route
                 exact
                 path="/portfolio/:slug"
--- src/components/navigation-container/navigation-container.js
+++ src/components/navigation-container/navigation-container.js
@@ -1,15 +1,32 @@
 import React from "react";
+import axios from "axios";
+import { withRouter } from "react-router";
 import { NavLink } from "react-router-dom";
 
-const NavigationComponent = (props) => {
+const NavigationComponent = props => {
   const dynamicLink = (route, linkText) => {
     return (
-    <div className="nav-link-wrapper">
-      <NavLink to="/blog" activeClassName="nav-link-active">
-        Blog
-      </NavLink>
-    </div>
+      <div className="nav-link-wrapper">
+        <NavLink to="/blog" activeClassName="nav-link-active">
+          Blog
+        </NavLink>
+      </div>
     );
+  };
+
+  const handleSignOut = () => {
+    axios
+      .delete("https://api.devcamp.space/logout", { withCredentials: true })
+      .then(response => {
+        if (response.status === 200) {
+          props.history.push("/");
+          props.handleSuccessfulLogout();
+        }
+        return response.data;
+      })
+      .catch(error => {
+        console.log("Error signing out", error);
+      });
   };
 
   return (
@@ -20,25 +37,32 @@ const NavigationComponent = (props) => {
             Home
           </NavLink>
         </div>
+
         <div className="nav-link-wrapper">
           <NavLink to="/about-me" activeClassName="nav-link-active">
             About
           </NavLink>
         </div>
+
         <div className="nav-link-wrapper">
           <NavLink to="/contact" activeClassName="nav-link-active">
             Contact
           </NavLink>
         </div>
 
-        {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/blog", "Blog") : null}
+        {props.loggedInStatus === "LOGGED_IN" ? (
+          dynamicLink("/blog", "Blog")
+        ) : null}
       </div>
 
       <div className="right-side">
         Jacob Batterman
+        {props.loggedInStatus === "LOGGED_IN" ? (
+          <a onClick={handleSignOut}>Sign Out</a>
+        ) : null}
       </div>
     </div>
   );
-}
+};
 
-export default NavigationComponent;
+export default withRouter(NavigationComponent);
