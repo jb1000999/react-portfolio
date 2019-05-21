aee586b3efea677d8d84d57f2f0e6c8b50f70118
Jacob Batterman <ctrjcb@gmail.com>
2019-05-21 14:51:24 -0600

    put in the ability to connent to api on portfolio manager

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioItems: []
+    };
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://jacobbatterman.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true
+      })
+      .then(response  => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        });
+      })
+      .catch(error => {
+        console.log ("error in getPortfolioItems", error);
+      })
+  }
+
+    componentDidMount() {
+      this.getPortfolioItems();
+    }
   render() {
     return (
     <div className="portfolio-manager-wrapper">
       <div className="left-column">
-      <h1>PortfolioManager</h1>
+        <h1>PortfolioManager</h1>
       </div>
 
       <div className="right-column">
-      <h1>portfolio sidebar</h1>
+        <h1>portfolio sidebar</h1>
       </div>
     </div>
     );
