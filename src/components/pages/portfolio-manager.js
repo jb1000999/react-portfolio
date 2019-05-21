4a83cc5d6b6f45552fc44ca222cdc63bb15ad082
Jacob Batterman <ctrjcb@gmail.com>
2019-05-21 14:25:54 -0600

    added portfolio manager for when signed in

---
+++ src/components/pages/portfolio-manager.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class PortfolioManager extends Component {
+  render() {
+    return (
+    <div className="portfolio-manager-wrapper">
+      <div className="left-column">
+      <h1>PortfolioManager</h1>
+      </div>
+
+      <div className="right-column">
+      <h1>portfolio sidebar</h1>
+      </div>
+    </div>
+    );
+  }
+}
