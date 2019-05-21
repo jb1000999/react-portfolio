403e784e901ff21c59ab0dfacf253a8dd67674a5
Jacob Batterman <ctrjcb@gmail.com>
2019-05-21 09:23:19 -0600

    refactored navigation container

--- src/components/navigation-container/navigation-container.js
+++ src/components/navigation-container/navigation-container.js
@@ -1,41 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 
-export default class NavigationComponent extends Component {
-  constructor() {
-    super();
-  }
+const NavigationComponent = (props) => {
 
-  render() {
-    return (
-      <div className="nav-wrapper">
-        <div className="left-side">
-          <div className="nav-link-wrapper">
-            <NavLink exact to="/" activeClassName="nav-link-active">
-              Home
-            </NavLink>
-          </div>
-          <div className="nav-link-wrapper">
-            <NavLink to="/about-me" activeClassName="nav-link-active">
-              About
-            </NavLink>
-          </div>
-          <div className="nav-link-wrapper">
-            <NavLink to="/contact" activeClassName="nav-link-active">
-              Contact
-            </NavLink>
-          </div>
-          <div className="nav-link-wrapper">
-            <NavLink to="/blog" activeClassName="nav-link-active">
-              Blog
-            </NavLink>
-          </div>
+  return (
+    <div className="nav-wrapper">
+      <div className="left-side">
+        <div className="nav-link-wrapper">
+          <NavLink exact to="/" activeClassName="nav-link-active">
+            Home
+          </NavLink>
         </div>
-
-        <div className="right-side">
-          Jacob Batterman
+        <div className="nav-link-wrapper">
+          <NavLink to="/about-me" activeClassName="nav-link-active">
+            About
+          </NavLink>
+        </div>
+        <div className="nav-link-wrapper">
+          <NavLink to="/contact" activeClassName="nav-link-active">
+            Contact
+          </NavLink>
+        </div>
+        <div className="nav-link-wrapper">
+          <NavLink to="/blog" activeClassName="nav-link-active">
+            Blog
+          </NavLink>
         </div>
       </div>
-    );
-  }
+
+      <div className="right-side">
+        Jacob Batterman
+      </div>
+    </div>
+  );
 }
+
+export default NavigationComponent;
