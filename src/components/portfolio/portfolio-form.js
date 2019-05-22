4a4cf22e642eea5c0a437634abf8a873b579973b
Jacob Batterman <ctrjcb@gmail.com>
2019-05-22 13:26:29 -0600

    started working on form functionality

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,11 +1,79 @@
 import React, {Component} from 'react';
 
 export default class PortfolioForm extends Component {
+  constructor (props) {
+    super (props);
+
+    this.state = {
+      name: '',
+      description: '',
+      category: '',
+      postition: '',
+      url: '',
+      thumb_image: '',
+      banner_image: '',
+      logo: '',
+    };
+
+    this.handleChange = this.handleChange.bind (this);
+    this.handleSubmit = this.handleSubmit.bind (this);
+  }
+
+  handleChange (event) {
+    this.setState ({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit (event) {
+    console.log ('event', event);
+    event.preventDefault ();
+  }
+
   render () {
     return (
       <div>
         <h1>PortfolioForm</h1>
-        <h1>PortfolioForm</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
+              type="text"
+              name="name"
+              placeholder="Portfolio Item Name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+
+            <input
+              type="text"
+              name="url"
+              placeholder="URL"
+              value={this.state.url}
+              onChange={this.handleChange}
+            />
+
+            <input
+              type="text"
+              name="category"
+              placeholder="Category"
+              value={this.state.url}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="description"
+              placeholder="Description"
+              value={this.state.url}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
     );
   }
