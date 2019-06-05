7aca8eed29edf5ca85ab892a1802fd8d1fc218d0
Jacob Batterman <ctrjcb@gmail.com>
2019-06-05 14:00:18 -0600

    added blog page content and connected api and pulling data from the api

---
+++ src/components/pages/blog-detail.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+export default class BLogDetail extends Component {
+  constructor (props) {
+    super (props);
+
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {},
+    };
+  }
+
+  getBlogItem () {
+    axios
+      .get (
+        `https://jacobbatterman.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
+      )
+      .then (response => {
+        this.setState ({
+          blogItem: response.data.portfolio_blog,
+        });
+      })
+      .catch (error => {
+        console.log ('getBlogItem error', error);
+      });
+  }
+
+  componentDidMount () {
+    this.getBlogItem ();
+  }
+
+  render () {
+    const {
+      title,
+      content,
+      featured_image_url,
+      blog_status,
+    } = this.state.blogItem;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <img src={featured_image_url} />
+        <div>
+          {content}
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+import BlogItem from '../blog/blog-item';
 
-export default function () {
-  return (
-    <div>
-      <h2>Blog</h2>
+class Blog extends Component {
+  constructor () {
+    super ();
 
-      <div>
-        <Link to="/about-me">Read more about myself</Link>
-      </div>
-    </div>
-  );
+    this.state = {
+      blogItems: [],
+    };
+
+    this.getBlogItems = this.getBlogItems.bind (this);
+  }
+
+  getBlogItems () {
+    axios
+      .get ('https://jacobbatterman.devcamp.space/portfolio/portfolio_blogs', {
+        withCredentials: true,
+      })
+      .then (response => {
+        this.setState ({
+          blogItems: response.data.portfolio_blogs,
+        });
+      })
+      .catch (error => {
+        console.log ('getBlogItems error', error);
+      });
+  }
+
+  componentWillMount () {
+    this.getBlogItems ();
+  }
+
+  render () {
+    const blogRecords = this.state.blogItems.map (blogItem => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+    });
+    return <div>{blogRecords}</div>;
+  }
 }
+
+export default Blog;
