e8388a353e55ae0fd88369e389c1e85ed008b6b4
Jacob Batterman <ctrjcb@gmail.com>
2019-06-05 15:59:34 -0600

    added styles to blog and started implementing infinite scroll feature

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -38,11 +38,16 @@ export default class BLogDetail extends Component {
       blog_status,
     } = this.state.blogItem;
     return (
-      <div>
-        <h1>{title}</h1>
-        <img src={featured_image_url} />
-        <div>
-          {content}
+      <div className="blog-container">
+        <div className="content-container">
+          <h1>{title}</h1>
+
+          <div className="featured-image-wrapper">
+            <img src={featured_image_url} />
+          </div>
+          <div className="content">
+            {content}
+          </div>
         </div>
       </div>
     );
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -9,12 +9,31 @@ class Blog extends Component {
 
     this.state = {
       blogItems: [],
+      totalCount: 0,
+      currentPage: 0,
     };
 
     this.getBlogItems = this.getBlogItems.bind (this);
+    this.activateInfiniteScroll ();
+  }
+
+  activateInfiniteScroll () {
+    window.onscroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        console.log ('get more posts');
+        2;
+      }
+    };
   }
 
   getBlogItems () {
+    this.setState ({
+      currentPage: this.state.currentPage + 1,
+    });
+
     axios
       .get ('https://jacobbatterman.devcamp.space/portfolio/portfolio_blogs', {
         withCredentials: true,
@@ -22,6 +41,7 @@ class Blog extends Component {
       .then (response => {
         this.setState ({
           blogItems: response.data.portfolio_blogs,
+          totalCount: response.data.meta.total_records,
         });
       })
       .catch (error => {
@@ -37,7 +57,14 @@ class Blog extends Component {
     const blogRecords = this.state.blogItems.map (blogItem => {
       return <BlogItem key={blogItem.id} blogItem={blogItem} />;
     });
-    return <div>{blogRecords}</div>;
+
+    return (
+      <div className="blog-container">
+        <div className="content-container">
+          {blogRecords}
+        </div>
+      </div>
+    );
   }
 }
 
