b0dab5d5e0b7ba600e5a7d71c6005fb805a6bb4f
Jacob Batterman <ctrjcb@gmail.com>
2019-09-05 15:32:25 -0600

    added modal in blog

---
+++ src/components/blog/blog-form.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+export default class BlogForm extends Component {
+  constructor (props) {
+    super (props);
+
+    this.state = {
+      title: '',
+      blog_status: '',
+    };
+
+    this.handleChange = this.handleChange.bind (this);
+    this.handleSubmit = this.handleSubmit.bind (this);
+  }
+
+  buildForm () {
+    let formData = new FormData ();
+
+    formData.append ('portfolio_blog[title]', this.state.title);
+    formData.append ('portfolio_blog[blog_status]', this.state.blog_status);
+
+    return formData;
+  }
+
+  handleSubmit (event) {
+    axios
+      .post (
+        'https://jacobbatterman.devcamp.space/portfolio/portfolio_blogs',
+        this.buildForm (),
+        {withCredentials: true}
+      )
+      .then (response => {
+        this.props.handleSuccessfullFormSubmission (response.data);
+      })
+      .catch (error => {
+        console.log ('handleSubmit for blog error', error);
+      });
+
+    event.preventDefault ();
+  }
+
+  handleChange (event) {
+    this.setState ({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          name="title"
+          placeholder="Blog Title"
+          value={this.state.title}
+        />
+
+        <input
+          type="text"
+          onChange={this.handleChange}
+          name="blog_status"
+          placeholder="Blog status"
+          value={this.state.blog_status}
+        />
+
+        <button>Save</button>
+      </form>
+    );
+  }
+}
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 import BlogItem from '../blog/blog-item';
+import BlogModal from '../modals/blog-modal';
 
 class Blog extends Component {
   constructor () {
@@ -13,28 +14,42 @@ class Blog extends Component {
       totalCount: 0,
       currentPage: 0,
       isLoading: true,
+      blogModalIsOpen: false,
     };
 
     this.getBlogItems = this.getBlogItems.bind (this);
-    this.activateInfiniteScroll ();
+    this.onScroll = this.onScroll.bind (this);
+    window.addEventListener ('scroll', this.onScroll, false);
+    this.handleNewBlogClick = this.handleNewBlogClick.bind (this);
+    this.handleModalClose = this.handleModalClose.bind (this);
   }
 
-  activateInfiniteScroll () {
-    window.onscroll = () => {
-      if (
-        this.state.isLoading ||
-        this.state.blogItems.length === this.state.totalCount
-      ) {
-        return;
-      }
+  handleModalClose () {
+    this.setState ({
+      blogModalIsOpen: false,
+    });
+  }
 
-      if (
-        window.innerHeight + document.documentElement.scrollTop ===
-        document.documentElement.offsetHeight
-      ) {
-        this.getBlogItems ();
-      }
-    };
+  handleNewBlogClick () {
+    this.setState ({
+      blogModalIsOpen: true,
+    });
+  }
+
+  onScroll () {
+    if (
+      this.state.isLoading ||
+      this.state.blogItems.length === this.state.totalCount
+    ) {
+      return;
+    }
+
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      this.getBlogItems ();
+    }
   }
 
   getBlogItems () {
@@ -68,6 +83,10 @@ class Blog extends Component {
     this.getBlogItems ();
   }
 
+  componentWillUnmount () {
+    window.removeEventListener ('scroll', this.onScroll, false);
+  }
+
   render () {
     const blogRecords = this.state.blogItems.map (blogItem => {
       return <BlogItem key={blogItem.id} blogItem={blogItem} />;
@@ -75,6 +94,16 @@ class Blog extends Component {
 
     return (
       <div className="blog-container">
+        <BlogModal
+          handleModalClose={this.handleModalClose}
+          modalIsOpen={this.state.blogModalIsOpen}
+        />
+
+        <div className="new-blog-link">
+          <a onClick={this.handleNewBlogClick}>
+            Open Modal!!
+          </a>
+        </div>
 
         <div className="content-container">
           {blogRecords}
