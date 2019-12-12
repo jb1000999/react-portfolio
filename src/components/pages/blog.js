cea1bdf7aea3238760a3a6b24b1ecc8ee3ccff42
jb1000999 <ctrjcb@gmail.com>
2019-12-12 03:51:16 -0700

    added authorization to see the edit button

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -109,13 +109,13 @@ class Blog extends Component {
           handleModalClose={this.handleModalClose}
           modalIsOpen={this.state.blogModalIsOpen}
         />
-
-        <div className="new-blog-link">
-          <a onClick={this.handleNewBlogClick}>
-            <FontAwesomeIcon icon="plus-circle" />
-          </a>
-        </div>
-
+        {this.props.loggedInStatus === "LOGGED_IN" ? (
+          <div className="new-blog-link">
+            <a onClick={this.handleNewBlogClick}>
+              <FontAwesomeIcon icon="plus-circle" />
+            </a>
+          </div>
+        ) : null}
         <div className="content-container">{blogRecords}</div>
         {this.state.isLoading ? (
           <div className="content-loader">
