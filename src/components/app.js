2867b6d45034174ac176b2b9b766d6b9587e0786
jb1000999 <ctrjcb@gmail.com>
2019-12-12 03:26:55 -0700

    added styles to modal

--- src/components/app.js
+++ src/components/app.js
@@ -1,101 +1,109 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import axios from 'axios';
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faTrash,
   faSignOutAlt,
   faEdit,
   faMinusCircle,
   faCircleNotch,
-} from '@fortawesome/free-solid-svg-icons';
+  faPlusCircle
+} from "@fortawesome/free-solid-svg-icons";
 
-import NavigationContainer from './navigation-container/navigation-container';
-import Home from './pages/home';
-import About from './pages/about';
-import Contact from './pages/contact';
-import Blog from './pages/blog';
-import BlogDetail from './pages/blog-detail';
-import PortfolioManager from './pages/portfolio-manager';
-import PortfolioDetail from './portfolio/portfolio-detail';
-import Auth from './pages/auth';
-import NoMatch from './pages/no-match';
+import NavigationContainer from "./navigation-container/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import BlogDetail from "./pages/blog-detail";
+import PortfolioManager from "./pages/portfolio-manager";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import Auth from "./pages/auth";
+import NoMatch from "./pages/no-match";
 
-library.add (faTrash, faSignOutAlt, faEdit, faMinusCircle, faCircleNotch);
+library.add(
+  faTrash,
+  faSignOutAlt,
+  faEdit,
+  faMinusCircle,
+  faCircleNotch,
+  faPlusCircle
+);
 
 export default class App extends Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
 
     this.state = {
-      loggedInStatus: 'NOT_LOGGED_IN',
+      loggedInStatus: "NOT_LOGGED_IN"
     };
 
-    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind (this);
-    this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind (this);
-    this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind (this);
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+    this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
+    this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this);
   }
 
-  handleSuccessfulLogin () {
-    this.setState ({
-      loggedInStatus: 'LOGGED_IN',
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
     });
   }
 
-  handleUnsuccessfulLogin () {
-    this.setState ({
-      loggedInStatus: 'NOT_LOGGED_IN',
+  handleUnsuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
     });
   }
 
-  handleSuccessfulLogout () {
-    this.setState ({
-      loggedInStatus: 'NOT_LOGGED_IN',
+  handleSuccessfulLogout() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
     });
   }
 
-  checkLoginStatus () {
+  checkLoginStatus() {
     return axios
-      .get ('https://api.devcamp.space/logged_in', {
-        withCredentials: true,
+      .get("https://api.devcamp.space/logged_in", {
+        withCredentials: true
       })
-      .then (response => {
+      .then(response => {
         const loggedIn = response.data.logged_in;
         const loggedInStatus = this.state.loggedInStatus;
 
-        if (loggedIn && loggedInStatus === 'LOGGED_IN') {
+        if (loggedIn && loggedInStatus === "LOGGED_IN") {
           return loggedIn;
-        } else if (loggedIn && loggedInStatus === 'NOT_LOGGED_IN') {
-          this.setState ({
-            loggedInStatus: 'LOGGED_IN',
+        } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "LOGGED_IN"
           });
-        } else if (!loggedIn && loggedInStatus === 'LOGGED_IN') {
-          this.setState ({
-            loggedInStatus: 'NOT_LOGGED_IN',
+        } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN"
           });
         }
       })
-      .catch (error => {
-        console.log ('Error', error);
+      .catch(error => {
+        console.log("Error", error);
       });
   }
 
-  componentDidMount () {
-    this.checkLoginStatus ();
+  componentDidMount() {
+    this.checkLoginStatus();
   }
 
-  authorizedPages () {
+  authorizedPages() {
     return [
       <Route
         key="portfolio-manager"
         path="/portfolio-manager"
         component={PortfolioManager}
-      />,
+      />
     ];
   }
 
-  render () {
+  render() {
     return (
       <div className="container">
         <Router>
@@ -123,8 +131,8 @@ export default class App extends Component {
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
               <Route path="/b/:slug" component={BlogDetail} />
-              {this.state.loggedInStatus === 'LOGGED_IN'
-                ? this.authorizedPages ()
+              {this.state.loggedInStatus === "LOGGED_IN"
+                ? this.authorizedPages()
                 : null}
               <Route
                 exact
